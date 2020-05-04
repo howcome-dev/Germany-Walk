13eb0761808ea343864c585bb8c27ef2a88dfa06
howcome-dev <monmari882@gmail.com>
2020-05-04 18:54:13 +0900

    mag.php作成

--- mag.php
+++ mag.php
@@ -11,33 +11,27 @@
     <?php include("assets/inc/header.php"); ?>
   </header>
   <main class="ly_wrapper">
-    <section class="ly_wrapper_conts">
+    <section class="ly_wrapper_conts bl_mag">
       <h2 class="ly_wrapper_ttl">メールマガジン登録</h2>
       <p>私の旅の予定をお送りします（無料）。<br>ぜひ、ご登録ください。</p>
-      <form action="#">
-        <table id="form-table">
-          <tr>
-            <td class="headeing"><label for="yourName">お名前</label></td>
-            <td><input type="text" name="yourName" id="yourName"></td>
-          </tr>
-          <tr>
-            <td class="heading"><label for="eMail">メールアドレス</label></td>
-            <td><input type="text" name="eMail" id="eMail"></td>
-          </tr>
-          <tr>
-            <td class="heading"><label for="message">メッセージ</label></td>
-            <td><textarea name="message" id="message" cols="40" rows="5"></textarea></td>
-          </tr>
-          <tr>
-            <td class="heading"></td>
-            <td><input type="submit" name="button" id="button" value="メールマガジンを登録する"></td>
-          </tr>
-        </table>
+      <form method="post">
+        <div>
+          <p><label for="yourName">お名前</label></p>
+          <p><input type="text" name="yourName" value="あなたの　おなまえ"></p>
+        </div>
+        <div>
+          <p><label for="eMail">メールアドレス</label></p>
+          <p><input type="text" name="eMail" value="i_love_deutsche_sprache@germany.walk"></p>
+        </div>
+        <div>
+          <p><label for="message">メッセージ</label></p>
+          <p><textarea name="message" value="応援メッセージを書いてください。お願いします。" cols="40" rows="5"></textarea></p>
+        </div>
+        <div>
+          <p><input type="submit" name="btn_confirm" value="入力内容を確認する"></p>
+        </div>
       </form>
     </section>
-    <aside class="bl_aside">
-      <?php include("assets/inc/aside.php"); ?>
-    </aside>
   </main>
   <footer>
     <?php include("assets/inc/footer.php"); ?>
