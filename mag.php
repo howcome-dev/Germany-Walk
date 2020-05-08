c637d1820524cb6b9a12bae8c716cff3f26354a2
howcome-dev <monmari882@gmail.com>
2020-05-09 01:56:09 +0900

    フォームのCSS調整

--- mag.php
+++ mag.php
@@ -1,5 +1,4 @@
 <?php
-//var_dump($_POST);
 
 // 変数の初期化
 $page_flag = 0;
@@ -66,53 +65,74 @@ if( !empty($_POST['btn_confirm']) ) {
   <main class="ly_wrapper">
     <section class="ly_wrapper_conts bl_mag">
       <h2 class="ly_wrapper_ttl">メールマガジン登録</h2>
-      <p>私の旅の予定をお送りします（無料）。<br>ぜひ、ご登録ください。</p>
+      <ol class="bl_mag_steps">
+        <li class="bl_mag_steps_item">
+          <span class="bl_mag_steps_itemNum">01</span>
+          <span class="bl_mag_steps_itemLabel">入力</span>
+        </li>
+        <li class="bl_mag_steps_item">
+          <span class="bl_mag_steps_itemNum">02</span>
+          <span class="bl_mag_steps_itemLabel">確認</span>
+        </li>
+        <li class="bl_mag_steps_item">
+          <span class="bl_mag_steps_itemNum">03</span>
+          <span class="bl_mag_steps_itemLabel">完了</span>
+        </li>
+      </ol>
+      <p class="bl_mag_txt">私の旅の予定をお送りします。<br>ぜひ、ご登録ください。</p>
       
       <?php if( $page_flag === 1): ?>
 
-      <form method="post" action="">
-        <div>
-          <p><label for="yourName">お名前</label></p>
-          <p><?php echo $_POST['yourName']; ?></p>
-        </div>
-        <div>
-          <p><label for="eMail">メールアドレス</label></p>
-          <p><?php echo $_POST['eMail']; ?></p>
-        </div>
-        <div>
-          <p><label for="message">メッセージ</label></p>
-          <p><?php echo $_POST['message']; ?></p>
-        </div>
-        <div>
-          <p><input type="submit" name="btn_back" value="戻る"></p>
-          <p><input type="submit" name="btn_submit" value="送信"></p>
-          <p><input type="hidden" name="yourName" value="<?php echo $_POST['yourName']; ?>"></p>
-          <p><input type="hidden" name="eMail" value="<?php echo $_POST['eMail']; ?>"></p>
-        </div>
+      <form class="bl_mag_form" method="post" action="">
+        <fieldset class="bl_mag_form_ttl">
+          <legend>
+            <span class="bl_mag_form_labelTxt">お名前 Name</span>
+          </legend>
+          <?php echo $_POST['last_name']; ?>
+          <?php echo $_POST['first_name']; ?>
+        </fieldset>
+        <p class="bl_mag_form_ttl">
+          <span class="bl_mag_form_labelTxt">メールアドレス Mail</span>
+          <?php echo $_POST['email']; ?>
+        </p>
+        <p class="bl_mag_form_ttl">
+          <span class="bl_mag_form_labelTxt">メッセージ Message</span>
+          <?php echo $_POST['message']; ?>
+        </p>
+        <p class="bl_mag_form_submit">
+          <button class="ly_btn" name="btn_submit" type="button">送信</button>
+          <button class="ly_btn" name="btn_back" type="button">入力画面に戻る</button>
+        </p>
+        <p><input type="hidden" name="last_name" value="<?php echo $_POST['last_name']; ?>"></p>
+        <p><input type="hidden" name="first_name" value="<?php echo $_POST['first_name']; ?>"></p>
+        <p><input type="hidden" name="email" value="<?php echo $_POST['email']; ?>"></p>
+        <p><input type="hidden" name="message" value="<?php echo $_POST['message']; ?>"></p>
       </form>
 
       <?php elseif( $page_flag === 2 ): ?>
 
-      <p>登録とメッセージありがとうございます！</p>
+      <p>登録完了です！メッセージありがとうございました。</p>
 
       <?php else: ?>
 
-      <form method="post" action="">
-        <div>
-          <p><label for="yourName">お名前</label></p>
-          <p><input type="text" name="yourName" value="あなたの　おなまえ"></p>
-        </div>
-        <div>
-          <p><label for="eMail">メールアドレス</label></p>
-          <p><input type="text" name="eMail" value="i_love_deutsche_sprache@germany.walk"></p>
-        </div>
-        <div>
-          <p><label for="message">メッセージ</label></p>
-          <p><textarea name="message" value="応援メッセージを書いてください。お願いします。" cols="40" rows="5"></textarea></p>
-        </div>
-        <div>
-          <p><input type="submit" name="btn_confirm" value="入力内容を確認する"></p>
-        </div>
+      <form class="bl_mag_form" method="post" action="">
+        <fieldset class="bl_mag_form_ttl">
+          <legend>
+            <span class="bl_mag_form_labelTxt">お名前 Name</span>
+          </legend>
+          <input class="bl_mag_form_txtfield" name="last_name" type="text" placeholder="姓 Family Name">
+          <input class="bl_mag_form_txtfield" name="first_name" type="text" placeholder="名 First Name">
+        </fieldset>
+        <p class="bl_mag_form_ttl">
+          <span class="bl_mag_form_labelTxt">メールアドレス Mail</span>
+          <input class="bl_mag_form_txtfield" name="email" type="text" placeholder="xxxx@germanywalk.com">
+        </p>
+        <p class="bl_mag_form_ttl">
+          <span class="bl_mag_form_labelTxt">メッセージ Message</span>
+          <textarea class="bl_mag_form_txtarea" name="message" rows="8" placeholder="最大文字数1000字まで。熱い想いを語ってください。"></textarea>
+        <p class="bl_mag_form_submit">
+          <button class="ly_btn" name="btn_confirm" type="button">入力内容を確認する</button>
+        </p>
       </form>
 
       <?php endif; ?>
