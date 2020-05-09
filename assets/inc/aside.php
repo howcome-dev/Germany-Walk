96db287a0480e902ef69213a927afa8420572268
howcome-dev <monmari882@gmail.com>
2020-05-09 15:54:06 +0900

    ヘッダーnavと登録ボタンを部品化

--- assets/inc/aside.php
+++ assets/inc/aside.php
@@ -7,5 +7,5 @@
   <h3 class="bl_aside_ttl">アイスバインはベルリンの郷土料理</h3>
   <p class="bl_aside_txt">アイスバインは豚のお肉。ほろほろでおいしいです。でも、パッサウにはありません。ひとくくりにドイツ料理と言っても現地の人でも知らないと言われます。困った顔をされてしまいますのでお気をつけを。</p>
 </div>
-<p><a class="bl_aside_link" href="mag.php" id="mm">メールマガジン登録</a></p>
+<p class="bl_aside_btn"><a class="ly_btn bl_aside_link" href="mag.php">メールマガジン登録</a></p>
 <p class="bl_aside_notes">最新情報をメールでお届けします。</p>
--- assets/inc/header.php
+++ assets/inc/header.php
@@ -1,7 +1,7 @@
 <h1 class="bl_header_logo"><a class="bl_header_logoLink" href="index.php">Germany Walk</a></h1>
 <nav class="bl_header_nav">
-  <li class="bl_header_list"><a class="bl_header_link" href="about.php">ドイツの魅力</a></li>
-  <li class="bl_header_list"><a class="bl_header_link" href="event.php">観光イベント</a></li>
-  <li class="bl_header_list"><a class="bl_header_link" href="list.php">おすすめWalk</a></li>
+  <li class="bl_header_list"><a class="ly_btn bl_header_link" href="about.php">ドイツの魅力</a></li>
+  <li class="bl_header_list"><a class="ly_btn bl_header_link" href="event.php">観光イベント</a></li>
+  <li class="bl_header_list"><a class="ly_btn bl_header_link" href="list.php">おすすめWalk</a></li>
 </nav>
-<p class="bl_header_lede">ドイツの魅力を紹介します！</p>
+<p class="bl_header_lede">私が見たドイツをご紹介！</p>
